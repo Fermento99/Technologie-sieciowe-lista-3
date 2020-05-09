3135590fe3dd6e8ab1a3ec64462bd64dee55e56d
Fermento99 <pk.pawlak99@gmail.com>
2020-05-09 19:13:14 +0200

    fixed readme

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Pliki z zadanymi programami znajdują się w odpowiedznich folderach.
+Aby zobaczyc przykładowe użycie programów, należy uruchomić odpowiednio programy 'framer.py' i 'example.py'.
--- zad2/example.py
+++ zad2/example.py
@@ -11,17 +11,17 @@ from medium import Medium
 
 def main():
     cord = Medium(100)
-    cord.display()
     pc1 = Device(cord, 4)
     pc1.add_message('1'*100)
     pc1.add_message('1'*100)
     pc1.add_message('1'*100)
     pc2 = Device(cord, 70)
     pc2.add_message('1'*100)
-    pc2.add_message('1'*100')
-    pc2.add_message('1'*100')
+    pc2.add_message('1'*100)
+    pc2.add_message('1'*100)
 
     for i in range(1000):
+        print('iteration {}: '.format(i+1), end='')
         cord.display()
         pc2.work()
         pc1.work()
